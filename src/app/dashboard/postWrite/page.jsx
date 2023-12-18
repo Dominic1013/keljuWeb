447468d7673bbbe7fc83559f761b38a22f66b3fa
ET Huang <yt159357000@gmail.com>
2023-12-18 17:37:56 +0800

    testing TinyMCE

--- src/app/dashboard/postWrite/page.jsx
+++ src/app/dashboard/postWrite/page.jsx
@@ -103,7 +103,7 @@ const PostWrite = () => {
   const tinyInit = {
     language: "zh_TW", // local module
     plugins:
-      "ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
+      " anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount",
     toolbar:
       "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
     tinycomments_mode: "embedded",
